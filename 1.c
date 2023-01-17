eeda0eefe1629694866324790f5c32ba40365211
Mohit <Mohitsh7979@gmail.com>
2023-01-17 17:07:07 +0530

    cp question

--- 1.c
+++ 1.c
@@ -3,7 +3,7 @@
 
 
 int main() {
-    // Complete the code.
+ 
     int a;
     long b;
     char c;
---
+++ DSA questions/ifelse/whileloop.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    // int a, n;
+    // cin >> a >> n;
+
+    // while (a<=n)
+    // {
+
+    //     if(a%2==0){
+    //        cout<<a<<"\n";
+    //     }
+    //     a++;
+    // }
+
+    // while (a >= n)
+    // {
+
+    //     cout << a << "\n";
+
+    //     a--;
+    // }
+
+    // int i=0,j;
+    // while (i<=5)
+    // {
+    //     j=0;
+    //     while (j<=5)
+    //     {
+    //         cout<<"*";
+    //         j++;
+    //     }
+
+    //     i++;
+    //     cout<<"\n";
+
+    // }
+
+    int i = 1, j;
+    while (i <= 5)
+    {
+        j = 1;
+        while (j <= 5)
+        {
+            cout<<"|"<<j<<"|"<<"*"<<"|";
+            j++;
+        }
+
+        i++;
+        cout << "\n";
+    }
+
+    // 
+
+} // int a, n;
+  // cin >> a >> n;
